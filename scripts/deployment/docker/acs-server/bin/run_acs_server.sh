f4aad6b0459b658be76cd9c1a87991aea1884983
Ankit Choudhary <ankit.choudhary@razorpay.com>
2023-06-12 16:37:44 +0530

    Adding more variables in deployment module

--- scripts/deployment/docker/acs-server/bin/run_acs_server.sh
+++ scripts/deployment/docker/acs-server/bin/run_acs_server.sh
@@ -5,8 +5,21 @@ set -e
 
 # Constants
 JAVA_EXEC=/usr/bin/java
+JAVA_EXTRA_OPTS=""
+JAVA_GC_OPTS=""
+REMOTE_DEBUGGING_PORT=7081
 
-JAVA_OPTS="-Djava.net.preferIPv4Stack=true
+if [ "$ENVIRONMENT" = "prod" ]; then
+  JAVA_GC_OPTS="$JAVA_GC_OPTS -Xms10G -Xmx10G -XX:+UseG1GC"
+else
+  # Enabling remote debugging only in non-prod environment
+  echo "Enabling remote debugging."
+  JAVA_EXTRA_OPTS="$JAVA_EXTRA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:${REMOTE_DEBUGGING_PORT}"
+
+  JAVA_GC_OPTS="$JAVA_GC_OPTS -Xms2G -Xmx2G -XX:+UseG1GC"
+fi
+
+JAVA_OPTS="$JAVA_EXTRA_OPTS $JAVA_GC_OPTS -Djava.net.preferIPv4Stack=true
           -Dsun.jnu.encoding=UTF-8
           -Dfile.encoding=UTF-8
           -Duser.timezone=UTC
