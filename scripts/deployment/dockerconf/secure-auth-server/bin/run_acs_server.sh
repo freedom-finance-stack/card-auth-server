4be4be2c891238bec5735d54988a4d2f001933ad
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-07-24 18:48:25 +0530

    update

--- scripts/deployment/dockerconf/secure-auth-server/bin/run_acs_server.sh
+++ scripts/deployment/dockerconf/secure-auth-server/bin/run_acs_server.sh
@@ -9,7 +9,7 @@ JAVA_EXTRA_OPTS=""
 JAVA_GC_OPTS=""
 REMOTE_DEBUGGING_PORT=7081
 
-if [ "$ENVIRONMENT" = "prod" ]; then
+if [ "$SPRING_PROFILES_ACTIVE" = "prod" ]; then
   JAVA_GC_OPTS="$JAVA_GC_OPTS -Xms2G -Xmx2G -XX:+UseG1GC"
 else
   # Enabling remote debugging only in non-prod environment
@@ -23,7 +23,7 @@ JAVA_OPTS="$JAVA_EXTRA_OPTS $JAVA_GC_OPTS -Djava.net.preferIPv4Stack=true
           -Dsun.jnu.encoding=UTF-8
           -Dfile.encoding=UTF-8
           -Duser.timezone=UTC
-          -Dspring.profiles.active=$ENVIRONMENT
+          -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE
           --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
 
 # Starting Secure Auth ACS Server Service
