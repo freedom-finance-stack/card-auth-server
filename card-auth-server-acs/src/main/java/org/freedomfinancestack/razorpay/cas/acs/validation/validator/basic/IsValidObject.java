ad23cfa4bb3f01a25e3c5e8cf0d36bac2e677ffc
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-08-16 15:34:46 +0530

    reviewd comment resolved

--- card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/validation/validator/basic/IsValidObject.java
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/validation/validator/basic/IsValidObject.java
@@ -6,6 +6,10 @@ import org.freedomfinancestack.razorpay.cas.acs.validation.validator.Validator;
 import org.freedomfinancestack.razorpay.cas.contract.ThreeDSecureErrorCode;
 import org.freedomfinancestack.razorpay.cas.contract.Validatable;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class IsValidObject<T extends Validatable> implements Validator<T> {
     private static final IsValidObject INSTANCE = new IsValidObject();
 
