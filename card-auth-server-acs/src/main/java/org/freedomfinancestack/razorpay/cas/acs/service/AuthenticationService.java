3cbf182777a2423baefa60e73674654641316200
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-08-21 12:18:05 +0530

    removed comment

--- card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/AuthenticationService.java
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/AuthenticationService.java
@@ -1,18 +1,13 @@
-/*******************************************************************************
- * Copyright (C)  IZealiant Technologies 2017  - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- *
- * Author 				: Ashish Kirpan
- * Created Date 			: Aug 1, 2017
- ******************************************************************************/
-
 package org.freedomfinancestack.razorpay.cas.acs.service;
 
 import org.freedomfinancestack.razorpay.cas.acs.dto.AuthResponse;
 import org.freedomfinancestack.razorpay.cas.acs.dto.Authentication;
 import org.freedomfinancestack.razorpay.cas.acs.exception.acs.ACSException;
 
+/**
+ * AuthenticationService interface
+ * @param <T>
+ */
 public interface AuthenticationService<T extends Authentication> {
 
     public void preAuthenticate(T t) throws ACSException;
