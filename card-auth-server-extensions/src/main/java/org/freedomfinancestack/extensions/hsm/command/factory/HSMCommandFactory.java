303e7086d70c471a8d14e93d068c97a64ddb774f
Ankit Choudhary <ankit.choudhary@razorpay.com>
2023-08-22 14:50:06 +0530

    Removing cas-extensions module as using this as dependency

--- card-auth-server-extensions/src/main/java/org/freedomfinancestack/extensions/hsm/command/factory/HSMCommandFactory.java
+++
@@ -1,9 +0,0 @@
-package org.freedomfinancestack.extensions.hsm.command.factory;
-
-import org.freedomfinancestack.extensions.hsm.command.HSMCommand;
-import org.freedomfinancestack.extensions.hsm.command.enums.HSMCommandType;
-
-public interface HSMCommandFactory {
-
-    HSMCommand getHSMCommand(HSMCommandType hsmCommandType);
-}
