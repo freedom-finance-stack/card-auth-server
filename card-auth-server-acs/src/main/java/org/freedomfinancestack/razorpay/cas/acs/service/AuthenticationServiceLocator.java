5387d228a91de48871cee311e5501a25f5a2c123
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-09-26 12:31:16 +0530

    code clean up

---
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/AuthenticationServiceLocator.java
@@ -0,0 +1,60 @@
+package org.freedomfinancestack.razorpay.cas.acs.service;
+
+import java.math.BigDecimal;
+
+import org.freedomfinancestack.razorpay.cas.acs.exception.InternalErrorCode;
+import org.freedomfinancestack.razorpay.cas.acs.exception.threeds.OperationNotSupportedException;
+import org.freedomfinancestack.razorpay.cas.acs.utils.Util;
+import org.freedomfinancestack.razorpay.cas.dao.enums.AuthType;
+import org.freedomfinancestack.razorpay.cas.dao.model.ChallengeAuthTypeConfig;
+import org.freedomfinancestack.razorpay.cas.dao.model.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthenticationServiceLocator {
+    private final @Qualifier("OTPAuthenticationService") AuthenticationService
+            otpAuthenticationService;
+
+    public AuthenticationService locateTransactionAuthenticationService(
+            Transaction transaction, ChallengeAuthTypeConfig authConfig)
+            throws OperationNotSupportedException {
+        String purchaseAmount = null;
+        if (transaction.getTransactionPurchaseDetail() != null
+                && !Util.isNullorBlank(
+                        transaction.getTransactionPurchaseDetail().getPurchaseAmount())) {
+            purchaseAmount = transaction.getTransactionPurchaseDetail().getPurchaseAmount();
+        }
+        if (authConfig.getThresholdAuthType() != null
+                && !Util.isNullorBlank(purchaseAmount)
+                && new BigDecimal(purchaseAmount).compareTo(authConfig.getThreshold()) >= 0) {
+            return locateService(authConfig.getThresholdAuthType());
+        } else {
+            return locateService(authConfig.getDefaultAuthType());
+        }
+    }
+
+    public AuthenticationService locateService(AuthType authType)
+            throws OperationNotSupportedException {
+        AuthenticationService authenticationService = null;
+
+        switch (authType) {
+            case OTP:
+                authenticationService = otpAuthenticationService;
+                break;
+                //            case PASSWORD:
+                //                authenticationService = passwordAuthenticationServiceImpl;
+                //                break;
+            default:
+                throw new OperationNotSupportedException(
+                        InternalErrorCode.INVALID_CONFIG, "Invalid Auth Type");
+        }
+        return authenticationService;
+    }
+}
--- card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/FeatureService.java
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/FeatureService.java
@@ -1,23 +1,10 @@
 package org.freedomfinancestack.razorpay.cas.acs.service;
 
-import java.math.BigDecimal;
 import java.util.Map;
 
 import org.freedomfinancestack.razorpay.cas.acs.dto.AuthConfigDto;
-import org.freedomfinancestack.razorpay.cas.acs.exception.InternalErrorCode;
 import org.freedomfinancestack.razorpay.cas.acs.exception.acs.ACSDataAccessException;
-import org.freedomfinancestack.razorpay.cas.acs.exception.threeds.OperationNotSupportedException;
-import org.freedomfinancestack.razorpay.cas.acs.utils.Util;
-import org.freedomfinancestack.razorpay.cas.dao.enums.AuthType;
 import org.freedomfinancestack.razorpay.cas.dao.enums.FeatureEntityType;
-import org.freedomfinancestack.razorpay.cas.dao.model.ChallengeAuthTypeConfig;
-import org.freedomfinancestack.razorpay.cas.dao.model.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * The {@code FeatureService} interface is responsible for fetching the authentication configuration
@@ -30,48 +17,4 @@ import lombok.extern.slf4j.Slf4j;
 public interface FeatureService {
     AuthConfigDto getAuthenticationConfig(Map<FeatureEntityType, String> entityIdsByType)
             throws ACSDataAccessException;
-
-    @Component
-    @Slf4j
-    @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-    class AuthenticationServiceLocator {
-        private final @Qualifier("OTPAuthenticationService") AuthenticationService
-                otpAuthenticationService;
-
-        public AuthenticationService locateTransactionAuthenticationService(
-                Transaction transaction, ChallengeAuthTypeConfig authConfig)
-                throws OperationNotSupportedException {
-            String purchaseAmount = null;
-            if (transaction.getTransactionPurchaseDetail() != null
-                    && !Util.isNullorBlank(
-                            transaction.getTransactionPurchaseDetail().getPurchaseAmount())) {
-                purchaseAmount = transaction.getTransactionPurchaseDetail().getPurchaseAmount();
-            }
-            if (authConfig.getThresholdAuthType() != null
-                    && !Util.isNullorBlank(purchaseAmount)
-                    && new BigDecimal(purchaseAmount).compareTo(authConfig.getThreshold()) >= 0) {
-                return locateService(authConfig.getThresholdAuthType());
-            } else {
-                return locateService(authConfig.getDefaultAuthType());
-            }
-        }
-
-        public AuthenticationService locateService(AuthType authType)
-                throws OperationNotSupportedException {
-            AuthenticationService authenticationService = null;
-
-            switch (authType) {
-                case OTP:
-                    authenticationService = otpAuthenticationService;
-                    break;
-                    //            case PASSWORD:
-                    //                authenticationService = passwordAuthenticationServiceImpl;
-                    //                break;
-                default:
-                    throw new OperationNotSupportedException(
-                            InternalErrorCode.INVALID_CONFIG, "Invalid Auth Type");
-            }
-            return authenticationService;
-        }
-    }
 }
