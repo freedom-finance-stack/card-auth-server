547ac3395eab0e8229977e32fcb2294924fe84bd
Ankit Choudhary <ankit.choudhary@razorpay.com>
2023-10-17 14:35:26 +0530

    Fixing review comments

--- card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/context/interceptor/InterceptorConfig.java
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/context/interceptor/InterceptorConfig.java
@@ -1,6 +1,5 @@
 package org.freedomfinancestack.razorpay.cas.acs.context.interceptor;
 
-import org.freedomfinancestack.extensions.externallibs.config.RequestInterceptorConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,13 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class InterceptorConfig implements WebMvcConfigurer {
 
-    private final RequestInterceptorConfig requestInterceptorConfig;
+    private final RequestInterceptor requestInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         try {
             log.info("addInterceptors() Adding registries");
-            registry.addInterceptor(new RequestInterceptor(requestInterceptorConfig)).order(1);
+            registry.addInterceptor(requestInterceptor).order(1);
             WebMvcConfigurer.super.addInterceptors(registry);
         } catch (Exception exception) {
             log.error(
