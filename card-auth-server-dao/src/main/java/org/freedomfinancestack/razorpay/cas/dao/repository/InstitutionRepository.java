7e4eae176960aad7933b5a274394e816831e4814
I5H <ishan.agrawal@razorpay.com>
2023-10-25 12:40:12 +0530

    removing access of Transactions tble from Spring data rest, as it was added for testing purpose

--- card-auth-server-dao/src/main/java/org/freedomfinancestack/razorpay/cas/dao/repository/InstitutionRepository.java
+++ card-auth-server-dao/src/main/java/org/freedomfinancestack/razorpay/cas/dao/repository/InstitutionRepository.java
@@ -3,7 +3,6 @@ package org.freedomfinancestack.razorpay.cas.dao.repository;
 import org.freedomfinancestack.razorpay.cas.dao.model.Institution;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 @Repository
 @RepositoryRestResource
--- card-auth-server-dao/src/main/java/org/freedomfinancestack/razorpay/cas/dao/repository/TransactionRepository.java
+++ card-auth-server-dao/src/main/java/org/freedomfinancestack/razorpay/cas/dao/repository/TransactionRepository.java
@@ -1,9 +1,7 @@
 package org.freedomfinancestack.razorpay.cas.dao.repository;
 
 import org.freedomfinancestack.razorpay.cas.dao.model.Transaction;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource
 public interface TransactionRepository extends BaseRepository<Transaction, String> {}
