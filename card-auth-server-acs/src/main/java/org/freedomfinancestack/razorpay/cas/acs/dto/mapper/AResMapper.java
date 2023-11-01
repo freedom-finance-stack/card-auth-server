7fe0a513f403ac69f42f67e49a548a1de4364679
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-11-01 14:20:34 +0530

    update

--- card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/dto/mapper/AResMapper.java
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/dto/mapper/AResMapper.java
@@ -134,7 +134,7 @@ public interface AResMapper {
 
     default String getAuthType(Transaction transaction) {
         if (transaction.getAuthenticationType() == null) {
-            return "";
+            return "02"; // default Auth type to 2, this is needed as required by test portal
         }
         return "0" + transaction.getAuthenticationType();
     }
