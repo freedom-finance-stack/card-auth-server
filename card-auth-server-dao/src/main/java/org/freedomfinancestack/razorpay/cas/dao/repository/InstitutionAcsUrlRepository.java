f22374480aa4e02fca009f29883e85b9fde433b6
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-11-16 17:37:16 +0530

    removed acs url table (#88)
    * removed acs url table
    
    * update

--- card-auth-server-dao/src/main/java/org/freedomfinancestack/razorpay/cas/dao/repository/InstitutionAcsUrlRepository.java
+++
@@ -1,9 +0,0 @@
-package org.freedomfinancestack.razorpay.cas.dao.repository;
-
-import org.freedomfinancestack.razorpay.cas.dao.model.InstitutionAcsUrl;
-import org.freedomfinancestack.razorpay.cas.dao.model.InstitutionAcsUrlPK;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstitutionAcsUrlRepository
-        extends BaseRepository<InstitutionAcsUrl, InstitutionAcsUrlPK> {}
