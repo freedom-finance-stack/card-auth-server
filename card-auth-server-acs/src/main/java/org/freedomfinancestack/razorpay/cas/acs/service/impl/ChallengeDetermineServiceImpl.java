20c7ed39ac20d3fb0bbef025319c64b5b30978e6
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-11-21 15:23:15 +0530

    challenge determine flow fixes and informational added

--- card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/impl/ChallengeDetermineServiceImpl.java
+++ card-auth-server-acs/src/main/java/org/freedomfinancestack/razorpay/cas/acs/service/impl/ChallengeDetermineServiceImpl.java
@@ -11,7 +11,10 @@ import org.freedomfinancestack.razorpay.cas.dao.enums.TransactionStatus;
 import org.freedomfinancestack.razorpay.cas.dao.model.Transaction;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ChallengeDetermineServiceImpl implements ChallengeDetermineService {
 
     private RiskFlag isChallengeRequired(final AREQ objAReq, final RiskFlag riskFlagByAcs) {
@@ -27,10 +30,27 @@ public class ChallengeDetermineServiceImpl implements ChallengeDetermineService 
                     || ThreeDSRequestorChallengeInd.WHITELIST_PROMPT_REQUESTED_IF_CHALLENGE_REQUIRED
                             .equals(challengeInd)) {
                 riskFlag = RiskFlag.CHALLENGE;
-            } else if (ThreeDSRequestorChallengeInd.DATA_SHARE_ONLY.equals(challengeInd)
-                    || ThreeDSRequestorChallengeInd.TRANSACTIONAL_RISK_ANALYSIS_IS_ALREADY_PERFORMED
-                            .equals(challengeInd)) {
+            } else if (Util.isNullorBlank(objAReq.getAcctInfo())
+                    && Util.isNullorBlank(objAReq.getAcctInfo().getSuspiciousAccActivity())) {
+                if ("02".equals(objAReq.getAcctInfo().getSuspiciousAccActivity())) {
+                    riskFlag = RiskFlag.CHALLENGE;
+                }
+            } else if (!Util.isNullorBlank(objAReq.getThreeDSReqAuthMethodInd())) {
+                ThreeDSReqAuthMethodInd methodInd =
+                        ThreeDSReqAuthMethodInd.getByValue(objAReq.getThreeDSReqAuthMethodInd());
+                if (ThreeDSReqAuthMethodInd.FAILED.equals(methodInd)
+                        || ThreeDSReqAuthMethodInd.NOT_PERFORMED.equals(methodInd)) {
+                    riskFlag = RiskFlag.CHALLENGE;
+                }
+            } else if (ThreeDSRequestorChallengeInd.TRANSACTIONAL_RISK_ANALYSIS_IS_ALREADY_PERFORMED
+                    .equals(challengeInd)) {
                 riskFlag = RiskFlag.NO_CHALLENGE;
+            } else if (ThreeDSRequestorChallengeInd.DATA_SHARE_ONLY.equals(challengeInd)) {
+                if (riskFlagByAcs.equals(RiskFlag.INFORMATIONAL)) {
+                    riskFlag = RiskFlag.INFORMATIONAL;
+                } else {
+                    riskFlag = RiskFlag.NO_CHALLENGE;
+                }
             } else if (!Util.isNullorBlank(objAReq.getThreeDSRequestorDecReqInd())
                     && objAReq.getThreeDSRequestorDecReqInd().equalsIgnoreCase("Y")) {
                 riskFlag = RiskFlag.DECOUPLED_CHALLENGE;
@@ -46,37 +66,13 @@ public class ChallengeDetermineServiceImpl implements ChallengeDetermineService 
             final AREQ objAReq, final Transaction transaction, final RiskFlag riskFlagAcs) {
 
         RiskFlag riskFlag = isChallengeRequired(objAReq, riskFlagAcs);
-        boolean challengeFlag = false;
         if (RiskFlag.CHALLENGE == riskFlag) {
-            challengeFlag = true;
+            transaction.setChallengeMandated(true);
+            transaction.setTransactionStatus(TransactionStatus.CHALLENGE_REQUIRED);
         } else if (RiskFlag.RBA == riskFlag) {
-            if (!Util.isNullorBlank(objAReq.getThreeDSRequestorChallengeInd())) {
-                ThreeDSRequestorChallengeInd challengeInd =
-                        ThreeDSRequestorChallengeInd.getByValue(
-                                objAReq.getThreeDSRequestorChallengeInd());
-                if (ThreeDSRequestorChallengeInd.CHALLENGE_REQUESTED_MANDATE.equals(challengeInd)
-                        || ThreeDSRequestorChallengeInd.CHALLENGE_REQUESTED_REQUESTER_PREFERENCE
-                                .equals(challengeInd)
-                        || ThreeDSRequestorChallengeInd.NO_PREFERENCE.equals(challengeInd)
-                        || ThreeDSRequestorChallengeInd
-                                .WHITELIST_PROMPT_REQUESTED_IF_CHALLENGE_REQUIRED
-                                .equals(challengeInd)) {
-                    challengeFlag = true;
-                }
-            } else if (Util.isNullorBlank(objAReq.getAcctInfo())
-                    && Util.isNullorBlank(objAReq.getAcctInfo().getSuspiciousAccActivity())) {
-                if ("02".equals(objAReq.getAcctInfo().getSuspiciousAccActivity())) {
-                    challengeFlag = true;
-                }
-            } else if (!Util.isNullorBlank(objAReq.getThreeDSReqAuthMethodInd())) {
-                ThreeDSReqAuthMethodInd methodInd =
-                        ThreeDSReqAuthMethodInd.getByValue(objAReq.getThreeDSReqAuthMethodInd());
-                if (ThreeDSReqAuthMethodInd.FAILED.equals(methodInd)
-                        || ThreeDSReqAuthMethodInd.NOT_PERFORMED.equals(methodInd)) {
-                    challengeFlag = true;
-                }
-            }
-
+            // todo add risk engine here
+            transaction.setChallengeMandated(true);
+            transaction.setTransactionStatus(TransactionStatus.CHALLENGE_REQUIRED);
         } else if (RiskFlag.DECOUPLED_CHALLENGE == riskFlag) {
             if (InternalConstants.YES.equals(objAReq.getThreeDSRequestorDecReqInd())) {
                 transaction.setTransactionStatus(TransactionStatus.CHALLENGE_REQUIRED_DECOUPLED);
@@ -86,12 +82,10 @@ public class ChallengeDetermineServiceImpl implements ChallengeDetermineService 
                 transaction.setTransactionStatus(TransactionStatus.SUCCESS);
                 transaction.setChallengeMandated(false);
             }
-        }
-
-        if (challengeFlag) {
-            transaction.setChallengeMandated(true);
-            transaction.setTransactionStatus(TransactionStatus.CHALLENGE_REQUIRED);
-        } else {
+        } else if (RiskFlag.INFORMATIONAL == riskFlag) {
+            transaction.setTransactionStatus(TransactionStatus.INFORMATIONAL);
+            transaction.setChallengeMandated(false);
+        } else if (RiskFlag.NO_CHALLENGE == riskFlag) {
             transaction.setTransactionStatus(TransactionStatus.SUCCESS);
             transaction.setChallengeMandated(false);
         }
