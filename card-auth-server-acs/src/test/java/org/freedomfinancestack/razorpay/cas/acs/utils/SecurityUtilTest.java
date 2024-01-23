e40b8fbf18268bf610543bca849bf547873f8f14
Varun Bansal <varun.bansal@razorpay.com>
2024-01-23 16:28:24 +0530

    Renaming functions

--- card-auth-server-acs/src/test/java/org/freedomfinancestack/razorpay/cas/acs/utils/SecurityUtilTest.java
+++ card-auth-server-acs/src/test/java/org/freedomfinancestack/razorpay/cas/acs/utils/SecurityUtilTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class SecurityUtilTest {
     @Test
-    public void test_generate_valid_EC_key_pair() throws SignerServiceException {
+    public void getPublicKey_SuccessCase() throws SignerServiceException {
         KeyPair keyPair = SecurityUtil.generateEphermalKeyPair();
 
         assertNotNull(keyPair);
@@ -26,7 +26,7 @@ class SecurityUtilTest {
     }
 
     @Test
-    public void test_nullKeyPair_throwsException() {
+    public void getPublicKey_NullPointerException() {
         // Arrange
         KeyPair keyPair = null;
 
@@ -35,7 +35,7 @@ class SecurityUtilTest {
     }
 
     @Test
-    public void test_invalidKeyPair_throwsException() {
+    public void getPublicKey_ClassCase_ExceptionCase() {
         // Arrange
         KeyPair keyPair = mock(KeyPair.class);
         PublicKey publicKey = mock(PublicKey.class);
@@ -44,89 +44,4 @@ class SecurityUtilTest {
         // Act and Assert
         assertThrows(ClassCastException.class, () -> SecurityUtil.getPublicKey(keyPair));
     }
-
-    //    @Test
-    //    public void getECKey() {
-    //        EphemPubKey ephemPubKey = new EphemPubKey();
-    //        ephemPubKey.setCrv("P-256");
-    //        ephemPubKey.setX("qT6cvxue8s2I4a-v50n7WvNWlELH_YTXo_eC4Zho0FM");
-    //        ephemPubKey.setY("5bAVo1XQ9u68cv0t_5JYFf8R2OuVYByj5h_dGKln-gs");
-    //
-    //        ECKey ecKey = SecurityUtil.getECKey(ephemPubKey);
-    //        assertNotNull(ecKey);
-    //    }
-    //
-    //    @Test
-    //    public void test_successfully_load_keystore_and_get_RSA_key_pair() throws Exception {
-    //        // Mock dependencies
-    //        SignerDetail signerDetail = mock(SignerDetail.class);
-    //        List<Base64> x509CertChain = new ArrayList<>();
-    //        x509CertChain.add(new Base64("Dummt"));
-    //
-    //        // Set up mock behavior
-    //        String keyPass = "password";
-    //        String keyStore = "keystore.jks";
-    //        String signerKeyPair = "keyPair";
-    //        RSAPublicKey publicKey = mock(RSAPublicKey.class);
-    //        RSAPrivateKey privateKey = mock(RSAPrivateKey.class);
-    //
-    //        when(signerDetail.getKeypass()).thenReturn(keyPass);
-    //        when(signerDetail.getKeystore()).thenReturn(keyStore);
-    //        when(signerDetail.getSignerKeyPair()).thenReturn(signerKeyPair);
-    //
-    //        X509Certificate rsaPublicKey = mock(X509Certificate.class);
-    //        RSAPrivateKey rsaPrivateKey = mock(RSAPrivateKey.class);
-    //        when(X509CertUtils.parse((byte[]) any())).thenReturn(rsaPublicKey);
-    //
-    //
-    //        // Invoke the method
-    //        KeyPair result = SecurityUtil.getRSAKeyPairFromKeystore(signerDetail, x509CertChain);
-    //
-    //        // Verify the result
-    //        assertNotNull(result);
-    //        assertEquals(publicKey, result.getPublic());
-    //        assertEquals(privateKey, result.getPrivate());
-    //    }
-    //
-    //    @Test
-    //    public void test_generateDigitalSignatureWithPS256_ValidKeypairAndX5CCertificateChain()
-    // throws SignerServiceException {
-    //        // Arrange
-    //        KeyPair keyPair = mock(KeyPair.class);
-    //        List<Base64> x5c = new ArrayList<>();
-    //        x5c.add(Base64.encode("certificate".getBytes()));
-    //        String jwsPayload = "payload";
-    //
-    //        // Act
-    //        String result = SecurityUtil.generateDigitalSignatureWithPS256(keyPair, x5c,
-    // jwsPayload);
-    //
-    //        // Assert
-    //        assertNotNull(result);
-    //    }
-    //    @Test
-    //    public void test_generateDigitalSignatureWithPS256_NullOrEmptyX5CCertificateChain() {
-    //        // Arrange
-    //        KeyPair keyPair = mock(KeyPair.class);
-    //        String jwsPayload = "payload";
-    //        RSAPrivateKey rsaPrivateKey = mock(RSAPrivateKey.class);
-    //        when(keyPair.getPrivate()).thenReturn(rsaPrivateKey);
-    //        RSASSASigner signer = mock(RSASSASigner.class);
-    //        when(new RSASSASigner(rsaPrivateKey)).thenReturn(signer);
-    //
-    //
-    //
-    //
-    //
-    //        // Act & Assert
-    //        assertThrows(SignerServiceException.class, () -> {
-    //            SecurityUtil.generateDigitalSignatureWithPS256(keyPair, null, jwsPayload);
-    //        });
-    //
-    //        assertThrows(SignerServiceException.class, () -> {
-    //            SecurityUtil.generateDigitalSignatureWithPS256(keyPair, new ArrayList<>(),
-    // jwsPayload);
-    //        });
-    //    }
-
 }
